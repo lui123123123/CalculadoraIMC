8665512bb399db7f802f2d9eb9e09cfed310afa6
Marian Morales <marianmoralesreyes6@gmail.com>
2024-04-18 16:27:48 -0700

    definir la logica para calcular el IMC, cucaaaas

--- CalculadoraIMC/PERSONA.cs
+++ CalculadoraIMC/PERSONA.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +10,16 @@ namespace CalculadoraIMC
     internal class PERSONA
     {
         public string Nombre { get; set; }
-        public string Edad { get; set;}
-        public string Estatura { get; set;}
-        public string Peso { get; set;}
+        public int Edad { get; set;}
+        public float Estatura { get; set;}
+        public float Peso { get; set;}
+
+
+        public void CalcularIMC()
+        {
+            var imc = Peso / (Estatura * Estatura);
+            Console.WriteLine($"El IMC de {Nombre} es {imc}" );
     }
+    }
+
 }
